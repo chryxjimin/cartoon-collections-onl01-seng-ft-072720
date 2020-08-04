60da2f82bdfc10e60f82c0e88a0f6e223bd58acf
Christie Yoon <cleex1999@gmail.com>
2020-08-04 18:26:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,3 +1,4 @@
+require 'pry'
 def roll_call_dwarves(names)
   names.each_with_index do |name, index|
   puts "#{index+1}. #{name}"
@@ -19,10 +20,10 @@ end
 def find_the_cheese(ingredients)
   # the array below is here to help
     new_array = []
+    #binding.pry
     cheese_types = ["cheddar", "gouda", "camembert"]
      if ingredients.include?(cheese_types)
      new_array << ingredients.find do |cheese|
     end
-     return new_array
   end
 end
